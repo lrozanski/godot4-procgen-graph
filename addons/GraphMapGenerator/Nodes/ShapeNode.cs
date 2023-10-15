cea83fd78cec39b8e8d458367a46095f17273e6a
Lech Różański <l.rozanski88@gmail.com>
2023-10-15 19:37:51 +0200

    Fixed ShapeNode.cs

--- addons/GraphMapGenerator/Nodes/ShapeNode.cs
+++ addons/GraphMapGenerator/Nodes/ShapeNode.cs
@@ -18,41 +18,14 @@ public partial class ShapeNode : GraphNode {
     }
 
     [Export]
-    public Rect2I Bounds {
-        get => bounds;
-        set {
-            bounds = value;
-
-            if (xSpinBox != null && ySpinBox != null && widthSpinBox != null && heightSpinBox != null) {
-                xSpinBox.Value = bounds.Position.X;
-                ySpinBox.Value = bounds.Position.Y;
-                widthSpinBox.Value = bounds.Size.X;
-                heightSpinBox.Value = bounds.Size.Y;
-            }
-        }
-    }
+    public Rect2I Bounds { get; set; }
 
     private ShapeType shapeType;
-    private Rect2I bounds;
-
     private OptionButton shapeTypeDropdown;
-    private SpinBox xSpinBox;
-    private SpinBox ySpinBox;
-    private SpinBox widthSpinBox;
-    private SpinBox heightSpinBox;
 
     public override void _EnterTree() {
         shapeTypeDropdown = GetNode<OptionButton>("ShapeType/OptionButton");
-        xSpinBox = GetNode<SpinBox>("X/SpinBox");
-        ySpinBox = GetNode<SpinBox>("Y/SpinBox");
-        widthSpinBox = GetNode<SpinBox>("Width/SpinBox");
-        heightSpinBox = GetNode<SpinBox>("Height/SpinBox");
-
         shapeTypeDropdown.ItemSelected += index => shapeType = Enum.GetValues<ShapeType>()[index];
-        xSpinBox.ValueChanged += value => bounds = new Rect2I((int) value, bounds.Position.Y, bounds.Size.X, bounds.Size.Y);
-        ySpinBox.ValueChanged += value => bounds = new Rect2I(bounds.Position.X, (int) value, bounds.Size.X, bounds.Size.Y);
-        widthSpinBox.ValueChanged += value => bounds = new Rect2I(bounds.Position.X, bounds.Position.Y, (int) value, bounds.Size.Y);
-        heightSpinBox.ValueChanged += value => bounds = new Rect2I(bounds.Position.X, bounds.Position.Y, bounds.Size.X, (int) value);
     }
 
 }
